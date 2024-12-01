32a8a19a909f32734f9064f08513eda466dc522f
nguyenconvapor <nguyenconvapor@gmail.com>
2024-12-01 12:32:18 +0700

    up

--- nodeload.sh
+++ nodeload.sh
@@ -5,5 +5,5 @@ script_dir="/root/node"
 
 # Vòng lặp để chạy từng script
 for i in {1..12}; do
-    gnome-terminal --tab --title "Desktop $i" -- bash -c "cd $script_dir; python3 dt$i.py; exec bash" &
+    gnome-terminal --tab --title "node $i" -- bash -c "cd $script_dir; python3 node$i.py; exec bash" &
 done
